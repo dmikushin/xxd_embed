c9f52f6d3de1ba15ad6beaffc0616ac4f3899579
Dmitry Mikushin <dmitry@kernelgen.org>
2022-03-05 15:12:28 +0100

    Do not use xdg-mime when it's not available

--- cmake/EmbedFile.cmake
+++ cmake/EmbedFile.cmake
@@ -5,7 +5,12 @@ file(READ ${FILE_HEX} CONTENT_HEX)
 
 string(SHA256 FILE_NAME_HASH ${FILE_KEY})
 
-execute_process(COMMAND xdg-mime query filetype ${FILE_PATH} OUTPUT_VARIABLE FILE_MIME)
+find_program(XDG_MIME xdg-mime)
+if (XDG_MIME)
+execute_process(COMMAND ${XDG_MIME} query filetype ${FILE_PATH} OUTPUT_VARIABLE FILE_MIME)
+else()
+set(FILE_MIME "unsupported")
+endif()
 
 # Substitute encoded HEX content into template source file
 configure_file("${CMAKE_CURRENT_INCLUDE_DIR}/xxd.in" ${EMBED_FILE_PATH})
